db400c8cb0527cc51ce125d9fc02d17f8655ee81
yamasol <a80600080@yahoo.com.tw>
2014-06-30 03:26:54 +0800

    add framework
    add some code

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 </head>
 <body>
 
+<!-- Feature  -->
+<header>Header</header>
+
+<!-- Feature  -->
+<div id='content'>
+
 <h1>My Test Prj</h1>
 <p>Prj</p>
 <p>Prj</p>
@@ -15,6 +21,13 @@
 <p>Prj</p>
 <p>Prj</p>
 <p>Prj</p>
+
+
+</div>
+
+
+<!-- Feature  -->
+<footer>Footer</footer>
 
 </body>
 </html>
